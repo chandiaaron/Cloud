a3d2a918835ac0ae765af99a33dfbfc583b9fac6
Aaron Chandi <chandiaaron@gmail.com>
2025-02-25 12:40:14 +0000

    updating overall

--- my_scripts/full_app_deployment.sh
+++ my_scripts/full_app_deployment.sh
@@ -3,8 +3,13 @@
 # update packages
 sudo apt update -y 
 
+echo step 1 Done!
+
 # upgrade packages 
 sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
+
+# clone repo with app code
+git clone https://github.com/chandiaaron/nodejs20-sparta-test-app-2025.git
 
 # install ngnix 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install nginx -y
@@ -15,6 +20,9 @@ sudo DEBIAN_FRONTEND=noninteractive bash -c "curl -fsSL https://deb.nodesource.c
 # install nodejs20
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs
 
+# cd into repo dir
+cd nodejs2-sparta-test-app-2025
+
 # change dir
 cd app
 
@@ -22,7 +30,12 @@ cd app
 sudo npm install 
 
 # app start
-sudo npm start 
+# sudo npm start 
+# install pm2 - process manager package for nodejs apps
+sudo DEBIAN_FRONTEND=noninteractive npm install pm2 -g 
 
+# kill any running node processes that could interfere
+pm2 kill
 
-
+# run the app with pm2
+pm2 start app.js
