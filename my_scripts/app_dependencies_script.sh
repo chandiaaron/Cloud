1e50ecd5168ef548dc704dcb2977995338e69e48
Aaron Chandi <chandiaaron@gmail.com>
2025-02-25 11:15:57 +0000

    file name change

--- my_scripts/app_dependencies_script.sh
+++ my_scripts/app_dependencies_script.sh
@@ -12,7 +12,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install nginx -y
 # get the required version of nodejs
 sudo DEBIAN_FRONTEND=noninteractive bash -c "curl -fsSL https://deb.nodesource.com/setup_20.x | bash -"
 
- # npm install to set up app
+# npm install to set up app
 sudo npm install 
 
 # app start
